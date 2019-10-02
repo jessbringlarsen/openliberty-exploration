66895421d33bcc7727dced1f5e56ec8b5bae5eb9
jebla <jess@bring-larsen.dk>
2019-10-02 23:41:35 +0200

    Added configuration to cache

--- src/main/java/io/openliberty/caching/Service.java
+++ src/main/java/io/openliberty/caching/Service.java
@@ -1,17 +1,18 @@
 package io.openliberty.caching;
 
-import java.util.UUID;
-
 import javax.cache.annotation.CacheDefaults;
 import javax.cache.annotation.CacheKey;
 import javax.cache.annotation.CacheResult;
+import javax.ejb.Singleton;
+import java.util.UUID;
 
+@Singleton
 @CacheDefaults(cacheName = "io.openliberty.caching")
 public class Service {
 
     @CacheResult
     public String getARandomStringFrom(@CacheKey String name) {
         System.out.println("Cache miss!");
-        return UUID.fromString(name).toString();
+        return UUID.randomUUID().toString();
     }
 }
